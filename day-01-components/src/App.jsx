cce0ce14016dd0fb82059e917e5a9ff5729eb4a5
Y Bhavana Reddy <159869445+ybhavanareddy@users.noreply.github.com>
2026-03-20 10:15:13 +0530

    Initial commit

---
+++ day-01-components/src/App.jsx
@@ -0,0 +1,20 @@
+import ProductCard from "./components/ProductCard";
+
+function App(){
+
+  const product = {
+    title:"iPhone 15",
+    price: 79999,
+    image:"https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-13-01.jpg"
+
+  };
+
+  return (
+    <div>
+      <ProductCard product={product}/>
+    </div>
+    
+  )
+}
+
+export default App;
